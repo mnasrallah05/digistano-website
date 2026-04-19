e28d7a0a5118f5961a1968d2ae0e379a560e2106
Mohammad Nasrallah <mnasrallah05@gmail.com>
2026-04-19 13:09:14 +0400

    Add DigiStano favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/icon.png", // change if you have custom logo
+    icon: "/icon1.png", // change if you have custom logo
   },
 };
 
