80946c9560281aaeb417821a9140fddad37c08dc
Mohammad Nasrallah <mnasrallah05@gmail.com>
2026-04-19 14:23:26 +0400

    change Navbar favicon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center" onClick={closeAllMenus}>
           <img
-            src="/images/logo.png"
+            src="/images/digistano-logo.png"
             alt="DigiStano"
             className="h-11 w-auto object-contain sm:h-12"
           />
